5b35b384b862b00676fec8c460dd9eb73d55826e
Marco Granatiero <marco.granatiero@3wlab.it>
2014-11-11 17:24:21 +0100

    add nav drawer items

--- app/src/main/java/it/moondroid/materialdesigntest/MainActivity.java
+++ app/src/main/java/it/moondroid/materialdesigntest/MainActivity.java
@@ -1,7 +1,9 @@
 package it.moondroid.materialdesigntest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -10,9 +12,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    // symbols for navdrawer items (indices must correspond to array below). This is
+    // not a list of items that are necessarily *present* in the Nav Drawer; rather,
+    // it's a list of all possible items.
+    protected static final int NAVDRAWER_ITEM_CARD_VIEW = 0;
+    protected static final int NAVDRAWER_ITEM_CARD_LISTVIEW = 1;
+
+    // titles for navdrawer items (indices must correspond to the above)
+    private static final int[] NAVDRAWER_TITLE_RES_ID = new int[]{
+            R.string.navdrawer_item_cardview,
+            R.string.navdrawer_item_cardlist
+    };
+
+    // icons for navdrawer items (indices must correspond to above array)
+    private static final int[] NAVDRAWER_ICON_RES_ID = new int[] {
+            R.drawable.ic_drawer_social,  // My Schedule
+            R.drawable.ic_drawer_social  // Explore
+    };
+
+    // list of navdrawer items that were actually added to the navdrawer, in order
+    private ArrayList<Integer> mNavDrawerItems = new ArrayList<Integer>();
+
+    // delay to launch nav drawer item, to allow close animation to play
+    private static final int NAVDRAWER_LAUNCH_DELAY = 250;
+
+    // fade in and fade out durations for the main content when switching between
+    // different Activities of the app through the Nav Drawer
+    private static final int MAIN_CONTENT_FADEOUT_DURATION = 150;
+    private static final int MAIN_CONTENT_FADEIN_DURATION = 250;
 
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
@@ -36,13 +73,13 @@ public class MainActivity extends ActionBarActivity {
         drawer.setDrawerListener(toggle);
 
         if (savedInstanceState == null) {
-//            getFragmentManager().beginTransaction()
-//                    .add(R.id.container, CardViewFragment.newInstance())
-//                    .commit();
-              getFragmentManager().beginTransaction()
-                    .add(R.id.container, new CardListFragment())
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, CardViewFragment.newInstance())
                     .commit();
         }
+
+        // populate the nav drawer with the correct items
+        populateNavDrawer();
     }
 
     @Override
@@ -78,5 +115,105 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Populates the navigation drawer with the appropriate items. */
+    private void populateNavDrawer() {
+        mNavDrawerItems.clear();
+        mNavDrawerItems.add(NAVDRAWER_ITEM_CARD_VIEW);
+        mNavDrawerItems.add(NAVDRAWER_ITEM_CARD_LISTVIEW);
+        createNavDrawerItems();
+    }
+
+    private void createNavDrawerItems() {
+        ViewGroup mDrawerItemsListContainer = (ViewGroup) findViewById(R.id.navdrawer_items_list);
+        if (mDrawerItemsListContainer == null) {
+            return;
+        }
+
+        View[] mNavDrawerItemViews = new View[mNavDrawerItems.size()];
+        mDrawerItemsListContainer.removeAllViews();
+        int i = 0;
+        for (int itemId : mNavDrawerItems) {
+            mNavDrawerItemViews[i] = makeNavDrawerItem(itemId, mDrawerItemsListContainer);
+            mDrawerItemsListContainer.addView(mNavDrawerItemViews[i]);
+            View separator = getLayoutInflater().inflate(R.layout.navdrawer_separator, mDrawerItemsListContainer, false);
+            mDrawerItemsListContainer.addView(separator);
+            ++i;
+        }
+    }
+
+    private View makeNavDrawerItem(final int itemId, ViewGroup container) {
+        View view = getLayoutInflater().inflate(R.layout.navdrawer_item, container, false);
+
+        ImageView iconView = (ImageView) view.findViewById(R.id.icon);
+        TextView titleView = (TextView) view.findViewById(R.id.title);
+        int iconId = itemId >= 0 && itemId < NAVDRAWER_ICON_RES_ID.length ?
+                NAVDRAWER_ICON_RES_ID[itemId] : 0;
+        int titleId = itemId >= 0 && itemId < NAVDRAWER_TITLE_RES_ID.length ?
+                NAVDRAWER_TITLE_RES_ID[itemId] : 0;
+
+        // set icon and text
+        iconView.setVisibility(iconId > 0 ? View.VISIBLE : View.GONE);
+        if (iconId > 0) {
+            iconView.setImageResource(iconId);
+        }
+        titleView.setText(getString(titleId));
+
+        //formatNavDrawerItem(view, itemId, selected);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNavDrawerItemClicked(itemId);
+            }
+        });
+
+        return view;
+    }
+
+    private void onNavDrawerItemClicked(final int itemId) {
+
+        // launch the target Activity after a short delay, to allow the close animation to play
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToNavDrawerItem(itemId);
+            }
+        }, NAVDRAWER_LAUNCH_DELAY);
+
+        // change the active item on the list so the user can see the item changed
+        //setSelectedNavDrawerItem(itemId);
+
+        // fade out the main content
+        View mainContent = findViewById(R.id.container);
+        if (mainContent != null) {
+            mainContent.animate().alpha(0).setDuration(MAIN_CONTENT_FADEOUT_DURATION);
+        }
+
+        drawer.closeDrawer(Gravity.START);
+
+    }
+
+    private void goToNavDrawerItem(int item) {
+        Intent intent;
+        switch (item) {
+            case NAVDRAWER_ITEM_CARD_VIEW:
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, CardViewFragment.newInstance())
+                        .commit();
+                break;
+
+            case NAVDRAWER_ITEM_CARD_LISTVIEW:
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, new CardListFragment())
+                        .commit();
+                break;
+        }
+
+        View mainContent = findViewById(R.id.container);
+        mainContent.setAlpha(0);
+        mainContent.animate().alpha(1).setDuration(MAIN_CONTENT_FADEIN_DURATION);
     }
 }
